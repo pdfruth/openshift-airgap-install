defdd4f445f92bdb3ffc7ebef051b9a4b5350623
pdfruth <pat@patfruth.com>
2020-11-05 06:48:07 -0700

    Update 2-create-pull-secret.sh

--- airgap-install/phase2/2-create-pull-secret.sh
+++ airgap-install/phase2/2-create-pull-secret.sh
@@ -6,6 +6,4 @@ export PATH=.:$PATH
 
 oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
 
-mv ${LOCAL_SECRET_JSON} ${LOCAL_SECRET_JSON}.tmp
-
-cat ${LOCAL_SECRET_JSON}.tmp | tr -d "[:space:]" > ${LOCAL_SECRET_JSON}
+cat ${LOCAL_SECRET_JSON} | tr -d "[:space:]" > ${LOCAL_SECRET_JSON}.nowhitespace
