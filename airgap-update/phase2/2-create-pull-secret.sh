d4489f7fcd2d4e7509280e3170710a84ef22606a
pdfruth <pat@patfruth.com>
2020-11-05 06:50:05 -0700

    Update 2-create-pull-secret.sh
    create a pull secret without whitespace

--- airgap-update/phase2/2-create-pull-secret.sh
+++ airgap-update/phase2/2-create-pull-secret.sh
@@ -6,3 +6,4 @@ export PATH=.:$PATH
 
 oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
 
+cat ${LOCAL_SECRET_JSON} | tr -d "[:space:]" > ${LOCAL_SECRET_JSON}.nowhitespace
