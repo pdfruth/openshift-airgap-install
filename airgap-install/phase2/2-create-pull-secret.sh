0b054baa1ca5bb0a568b21e43813e2225b668d66
pdfruth <pat@patfruth.com>
2020-11-18 20:50:21 -0700

    Update 2-create-pull-secret.sh
    oc cli requires presence of a valid kube config.  We wont have one yet.
    So, fall back to generating a pull secret with raw json, rather than using oc cli.

--- airgap-install/phase2/2-create-pull-secret.sh
+++ airgap-install/phase2/2-create-pull-secret.sh
@@ -4,5 +4,11 @@ export PATH=.:$PATH
 
 . ./env
 
-oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
+# oc registry login only works if there is a valid kube config.
+# So, we will fall back to generating raw json instead.
+# oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
 
+REGISTRY_CREDS_B64=$(echo -n "${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}" | base64 -w0)
+cat << EOF > pull-secret-new.json
+{"auths":{"${LOCAL_REGISTRY}":{"auth":"$REGISTRY_CREDS_B64"}}}
+EOF
