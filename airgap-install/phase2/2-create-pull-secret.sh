8a19d97ebd0e3424a7a35d09137ad196b1e6a322
pdfruth <pat@patfruth.com>
2020-11-18 20:52:31 -0700

    Update 2-create-pull-secret.sh
    use variable instead of hard-coded file name

--- airgap-install/phase2/2-create-pull-secret.sh
+++ airgap-install/phase2/2-create-pull-secret.sh
@@ -9,6 +9,6 @@ export PATH=.:$PATH
 # oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
 
 REGISTRY_CREDS_B64=$(echo -n "${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}" | base64 -w0)
-cat << EOF > pull-secret-new.json
+cat << EOF > ${LOCAL_SECRET_JSON}
 {"auths":{"${LOCAL_REGISTRY}":{"auth":"$REGISTRY_CREDS_B64"}}}
 EOF
