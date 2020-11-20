c24efa155c80bc1efcacbbc93b5cf92539fa2c06
pdfruth <pat@patfruth.com>
2020-11-19 20:47:58 -0700

    Create 2-dry-run.sh

---
+++ airgap-install/2-dry-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export PATH=.:$PATH
+
+. ./env
+
+(oc adm -a ${LOCAL_SECRET_JSON} release mirror \
+     --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} \
+     --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} \
+     --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} --dry-run) 2>&1 | tee dry-run.out
