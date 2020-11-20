a2c3f109f01a3db754b96ec518db60810b4b8024
pdfruth <pat@patfruth.com>
2020-11-19 20:44:33 -0700

    Delete 2-update-pull-secret.sh

--- airgap-install/phase1/2-update-pull-secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=.:$PATH
-
-. ./env
-
-oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
-
