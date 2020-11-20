32eeafb64ce2792b23996d24d5904903ea0e88d8
pdfruth <pat@patfruth.com>
2020-11-19 20:47:24 -0700

    Create 1-download-tools.sh

---
+++ airgap-install/1-download-tools.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export PATH=.:$PATH
+
+. ./env
+
+#if [[ ${ARCHITECTURE} == "s390x" ]]
+#then
+  #wget https://mirror.openshift.com/pub/openshift-v4/${ARCHITECTURE}/dependencies/rhcos/4.6/latest/rhcos-live-initramfs.s390x.img
+  #wget https://mirror.openshift.com/pub/openshift-v4/${ARCHITECTURE}/dependencies/rhcos/4.6/latest/rhcos-live-kernel-s390x
+  #wget https://mirror.openshift.com/pub/openshift-v4/${ARCHITECTURE}/dependencies/rhcos/4.6/latest/rhcos-live-rootfs.s390x.img
+  #wget https://mirror.openshift.com/pub/openshift-v4/${ARCHITECTURE}/dependencies/rhcos/4.6/latest/rhcos-dasd.s390x.raw.gz
+  #wget https://mirror.openshift.com/pub/openshift-v4/${ARCHITECTURE}/dependencies/rhcos/4.6/latest/rhcos-metal.s390x.raw.gz
+#fi
+
+wget https://mirror.openshift.com/pub/openshift-v4/${ARCHITECTURE}/clients/ocp/${OCP_RELEASE}/openshift-client-linux-${OCP_RELEASE}.tar.gz
+wget https://mirror.openshift.com/pub/openshift-v4/${ARCHITECTURE}/clients/ocp/${OCP_RELEASE}/openshift-install-linux-${OCP_RELEASE}.tar.gz
+
+tar -xvzf openshift-client-linux-${OCP_RELEASE}.tar.gz
+#tar -xvzf openshift-install-linux-${OCP_RELEASE}.tar.gz
+
+REGISTRY_IMAGE_NAME_SHORT=$(echo ${REGISTRY_IMAGE_NAME} | sed "s/ibmcom\///g")
+podman pull ${REGISTRY_IMAGE_NAME}
+podman tag ${REGISTRY_IMAGE_NAME} ${REGISTRY_IMAGE_NAME_SHORT}
+podman save ${REGISTRY_IMAGE_NAME_SHORT} | gzip > registry.tar.gz
+cp registry.tar.gz phase2-scripts/extras
