7c32856ccace0160eba670e455cf50dcc88332ef
pdfruth <pat@patfruth.com>
2020-11-19 20:44:47 -0700

    Delete 4-make-portable-media.sh

--- airgap-install/phase1/4-make-portable-media.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PATH=.:$PATH
-
-. ./env
-
-oc adm release mirror -a ${LOCAL_SECRET_JSON} \
-     --to-dir=${REMOVABLE_MEDIA_PATH}/mirror quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE}
