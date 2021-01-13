843d8cf3d7e1422f5b09f3888d8bb32bbcf71e7c
pdfruth <pat@patfruth.com>
2021-01-12 19:30:53 -0700

    Update 1-unpack-media.sh

--- airgap-update/phase2-scripts/1-unpack-media.sh
+++ airgap-update/phase2-scripts/1-unpack-media.sh
@@ -4,8 +4,6 @@ export PATH=.:$PATH
 
 . ./env
 
-tar -xvzf airgap-${OCP_RELEASE}-${ARCHITECTURE}.tgz
-
 tar -xvzf openshift-client-linux-${OCP_RELEASE}.tar.gz
 tar -xvzf openshift-install-linux-${OCP_RELEASE}.tar.gz
 
