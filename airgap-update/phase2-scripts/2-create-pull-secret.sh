c9a686ef627d82d3c9c6161ff6ff43cd67d63759
pdfruth <pat@patfruth.com>
2021-01-12 19:29:30 -0700

    Update 2-create-pull-secret.sh

--- airgap-update/phase2-scripts/2-create-pull-secret.sh
+++ airgap-update/phase2-scripts/2-create-pull-secret.sh
@@ -4,6 +4,11 @@ export PATH=.:$PATH
 
 . ./env
 
-oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
+# oc registry login only works if there is a valid kube config.
+# So, we will fall back to generating raw json instead.
+# oc registry login --to ./${LOCAL_SECRET_JSON} --registry "${LOCAL_REGISTRY}" --auth-basic=${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}
 
-cat ${LOCAL_SECRET_JSON} | tr -d "[:space:]" > ${LOCAL_SECRET_JSON}.nowhitespace
+REGISTRY_CREDS_B64=$(echo -n "${LOCAL_REGISTRY_USER}:${LOCAL_REGISTRY_PASSWORD}" | base64 -w0)
+cat << EOF > ${LOCAL_SECRET_JSON}
+{"auths":{"${LOCAL_REGISTRY}":{"auth":"$REGISTRY_CREDS_B64"}}}
+EOF
