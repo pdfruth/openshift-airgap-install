7859e9d5a3ebcede6e595d1e832aa481e7ca320c
pdfruth <pat@patfruth.com>
2021-09-10 06:23:23 -0600

    Update 3a-start-registry.sh

--- airgap-install/phase2-scripts/extras/mirror-registry/3a-start-registry.sh
+++ airgap-install/phase2-scripts/extras/mirror-registry/3a-start-registry.sh
@@ -13,7 +13,7 @@ Description=mirror registry container in systemd
 Environment="BASEDIR=/home/ocpadmin/mirror-registry"
 Restart=on-failure
 ExecStartPre=/usr/bin/rm -f /%t/%n-pid /%t/%n-cid
-ExecStart=/usr/bin/podman run --conmon-pidfile /%t/%n-pid --cidfile /%t/%n-cid --name mirror-registry -p 5000:5000 -v ${BASEDIR}/data:/var/lib/registry:z -v ${BASEDIR}/auth:/auth:z -v ${BASEDIR}/certs:/certs:z -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" -e "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd" -e "REGISTRY_HTTP_TLS_CERTIFICATE=/certs/mirror-registry.crt" -e "REGISTRY_HTTP_TLS_KEY=/certs/mirror-registry.key" -d ibmcom/registry-s390x:2.6.2.5
+ExecStart=/usr/bin/podman run --conmon-pidfile /%t/%n-pid --cidfile /%t/%n-cid --name mirror-registry -p 5000:5000 -v ${BASEDIR}/data:/var/lib/registry:z -v ${BASEDIR}/auth:/auth:z -v ${BASEDIR}/certs:/certs:z -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" -e "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd" -e "REGISTRY_HTTP_TLS_CERTIFICATE=/certs/mirror-registry.crt" -e "REGISTRY_HTTP_TLS_KEY=/certs/mirror-registry.key" -e "REGISTRY_STORAGE_DELETE_ENABLED=true" -d ibmcom/registry-s390x:2.6.2.5
 ExecStop=/usr/bin/sh -c "/usr/bin/podman rm -f `cat /%t/%n-cid`"
 KillMode=none
 Type=forking
