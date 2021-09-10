987dc99109515b0f37b1614e935a9a8e728f5d39
pdfruth <pat@patfruth.com>
2021-09-10 06:24:14 -0600

    Update 3-start-registry.sh
    Enable delete

--- airgap-install/phase2-scripts/extras/mirror-registry/3-start-registry.sh
+++ airgap-install/phase2-scripts/extras/mirror-registry/3-start-registry.sh
@@ -13,4 +13,5 @@ podman run --name mirror-registry -p 5000:5000 \
      -e "REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd" \
      -e "REGISTRY_HTTP_TLS_CERTIFICATE=/certs/mirror-registry.crt" \
      -e "REGISTRY_HTTP_TLS_KEY=/certs/mirror-registry.key" \
+     -e "REGISTRY_STORAGE_DELETE_ENABLED=true" \
      -d ibmcom/registry-s390x:2.6.2.5
